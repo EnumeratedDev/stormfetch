341138116b4b3d9317064df8f5fdcf0f93a7cc44
EnumDev <enumdev@enumerated.dev>
2025-12-22 12:42:36 +0200

    Add default color map

--- src/color.go
+++ src/color.go
@@ -5,9 +5,13 @@ import (
 	"strings"
 )
 
-func setupColorMap(asciiArtHeader string) map[int]string {
-	colorMap := make(map[int]string)
-	colorMap[0] = "\033[0m"
+func setupColorMap(asciiArtHeader string) []string {
+	colorMap := make([]string, 10)
+
+	// Set default color map values
+	for i := range 10 {
+		colorMap[i] = "\033[0m"
+	}
 
 	// Return if header is empty
 	if asciiArtHeader == "" {
@@ -16,8 +20,8 @@ func setupColorMap(asciiArtHeader string) map[int]string {
 
 	// Read colors from ascii art header
 	ansiColors := strings.Split(strings.TrimPrefix(asciiArtHeader, "#/"), ";")
-	for i, ansiColor := range ansiColors {
-		colorMap[i+1] = fmt.Sprintf("\033[38;5;%sm", ansiColor)
+	for i := 0; i < 9 && i < len(ansiColors); i++ {
+		colorMap[i+1] = fmt.Sprintf("\033[38;5;%sm", ansiColors[i])
 	}
 
 	return colorMap
